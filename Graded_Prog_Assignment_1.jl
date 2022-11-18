9fd5d49a09e0c8de90834bcd28bd1eb7508c5698
abhijitchak103 <abhichaxz@gmail.com>
2022-11-18 20:56:12 +0530

    Added Programming Assignment 1

---
+++ Graded_Prog_Assignment_1.jl
@@ -0,0 +1,36 @@
+# 1. Write a Python program to print "Hello Python"?
+println("Hello Python")
+
+# 2. Write a Python program to do arithmetical operations addition and division.?
+
+function add(num1, num2)
+  return num1 + num2
+end
+
+function divide(num1, num2)
+  if num2 != 0
+    return num1 / num2
+  end
+end
+
+# 3. Write a Python program to find the area of a triangle?
+
+function triangle_area(height, base)
+  return 0.5*base*height^2  
+end
+
+# 4. Write a Python program to swap two variables?
+
+function swap(x, y)
+  z = y
+  y = x
+  x = z
+  return x, y
+end
+
+# 5. Write a Python program to generate a random number?
+
+#= 
+Unable to find correct implementation 
+to generate a random number
+=#
